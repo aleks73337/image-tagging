831bbf8a315db060417d0a01b0b0b8b4a133d45e
Aleksey Loginov <victimsnino@gmail.com>
2021-04-10 00:39:18 +0300

    make parser async-based

--- Parser/greedy_parser.py
+++ Parser/greedy_parser.py
@@ -4,6 +4,29 @@ import sys
 import json
 import re
 import logging
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+
+def patch_http_connection_pool(**constructor_kwargs):
+    from urllib3 import connectionpool, poolmanager
+
+    class MyHTTPConnectionPool(connectionpool.HTTPConnectionPool):
+        def __init__(self, *args,**kwargs):
+            kwargs.update(constructor_kwargs)
+            super(MyHTTPConnectionPool, self).__init__(*args,**kwargs)
+    poolmanager.pool_classes_by_scheme['http'] = MyHTTPConnectionPool
+
+def patch_https_connection_pool(**constructor_kwargs):
+    from urllib3 import connectionpool, poolmanager
+
+    class MyHTTPSConnectionPool(connectionpool.HTTPSConnectionPool):
+        def __init__(self, *args,**kwargs):
+            kwargs.update(constructor_kwargs)
+            super(MyHTTPSConnectionPool, self).__init__(*args,**kwargs)
+    poolmanager.pool_classes_by_scheme['https'] = MyHTTPSConnectionPool
+
+patch_http_connection_pool(maxsize=50)
+patch_https_connection_pool(maxsize=50)
 
 from tqdm import tqdm
 
@@ -41,6 +64,7 @@ class GreedyParser(Base):
         self.__min_tags_per_post    = min_tags_per_post
         self.__folder               = folder_to_save_data
         self.__meta_file_path       = os.path.join(self.__folder, '_meta.json')
+        self.__process_user_parallel = Parallel(n_jobs=4, require='sharedmem')
 
         if not os.path.exists(self.__folder):
             os.makedirs(self.__folder)
@@ -56,13 +80,19 @@ class GreedyParser(Base):
 
         self.__bar = tqdm(total=self.__total_count_of_posts)
 
-        while index < len(self.__requested_users):
-            if len(self.__posts) - initial_count >= self.__total_count_of_posts:
-                break
+        with Parallel(n_jobs=2, require='sharedmem') as parallel:
+            skip_save = False
+            while index < len(self.__requested_users):
+                if len(self.__posts) - initial_count >= self.__total_count_of_posts:
+                    break
+                tasks = []
+                if not skip_save:
+                    tasks.append(delayed(self.__save_meta)(index-1, self.__meta_file_path, self.__requested_users, self.__posts))
+                tasks.append(delayed(self.__process_user_name)(self.__requested_users[index]))
+                skip_save = parallel(tasks)[-1]
+                index += 1
 
-            self.__process_user_name(self.__requested_users[index])
-            self.__save_meta(index)
-            index += 1
+        self.__save_meta(index-1, self.__meta_file_path, self.__requested_users, self.__posts)
         self.__bar.close()
 
         self._logger.info(f"Collected {len(self.__posts) - initial_count} new post")
@@ -73,33 +103,48 @@ class GreedyParser(Base):
         self.__bar.set_description(f'Current user is {username}')
 
         if user.is_business_account or user.is_private:
-            return
+            return False
 
-        for post in _get_data(user.posts, self.__count_of_posts):
-            if not post.is_video:
-                self.__process_post(post)
+        objects = self.__process_user_parallel([delayed(_get_data)(user.posts, self.__count_of_posts), 
+                                                           delayed(_get_data)(user.follow, self.__cont_of_following)])
+                                    
+        posts = objects[0]
+        users = objects[1]
 
-        for username in _get_data(user.follow, self.__cont_of_following):
+        new_posts = self.__process_user_parallel(delayed(self.__process_post)(post, self.__min_tags_per_post, self.__folder) for post in posts)
+        new_posts = [data for data in new_posts if data]
+        self.__posts += new_posts
+        count_new_posts = len(new_posts)
+        self.__bar.update(count_new_posts)
+
+        for username in users:
             if username not in self.__requested_users:
                 self.__requested_users.append(username)
 
-    def __process_post(self, post : Post):
+        return count_new_posts != 0
+    
+    @staticmethod
+    def __process_post(post : Post, __min_tags_per_post : int, folder : str):
+        if post.is_video:
+            return None
+
         tags = tags_regular.findall(post.comment)
-        if len(tags) < self.__min_tags_per_post:
+        if len(tags) < __min_tags_per_post:
             return
         
-        self._logger.debug(f'Try to save {post.photo_url}')
+        #self._logger.debug(f'Try to save {post.photo_url}')
         try:
-            urllib.request.urlretrieve(post.photo_url, os.path.join(self.__folder, str(post.id)+'.jpg'))
-            self.__posts.append({'id': post.id, 'comment': post.comment, 'tags' : tags, 'accessibility_caption' : post.accessibility_caption})
-            self.__bar.update(1)
+            urllib.request.urlretrieve(post.photo_url, os.path.join(folder, str(post.id)+'.jpg'))
+            return {'id': post.id, 'comment': post.comment, 'tags' : tags, 'accessibility_caption' : post.accessibility_caption}
         except Exception as e:
-            self._logger.error(e)
+            #self._logger.error(e)
+            return None
 
-    def __save_meta(self, index):
-        self._logger.debug(f"Save... Index {index} Posts {len(self.__posts)} users {len(self.__requested_users)}")
-        with open(self.__meta_file_path, 'w') as file:
-            json.dump({'meta': {'last_index': index, 'users': self.__requested_users, 'posts_count' : len(self.__posts)}, 'data': self.__posts},
+    @staticmethod
+    def __save_meta(index, __meta_file_path:str, __requested_users, __posts ):
+        #self._logger.debug(f"Save... Index {index} Posts {len(self.__posts)} users {len(self.__requested_users)}")
+        with open(__meta_file_path, 'w') as file:
+            json.dump({'meta': {'last_index': index, 'users': __requested_users, 'posts_count' : len(__posts)}, 'data': __posts},
                     file,
                     indent=4)
     def __try_to_restore(self):
@@ -125,7 +170,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('root_user')
     parser.add_argument('--folder', type=str, required=False, default=os.path.join(parent_dir, 'dataset'))
-    parser.add_argument('--posts_per_profile', type=int, required=False, default=100)
+    parser.add_argument('--posts_per_profile', type=int, required=False, default=200)
     parser.add_argument('--following_per_profile', type=int, required=False, default=100)
     parser.add_argument('--total_count_of_posts', type=int, required=False, default=500)
     parser.add_argument('--min_tags_per_post', type=int, required=False, default=3)
