d625c6eb13d332124ae9762ce09b3259face816e
Aleks Somov <aleksandr.somov@zennolab.com>
2021-04-22 11:30:48 +0000

    Fix errors

--- utils/dataset_parser.py
+++ utils/dataset_parser.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 class DatasetParser:
     def __init__(self, data_folder : str):
@@ -7,18 +8,20 @@ class DatasetParser:
         self.imgs_paths, self.tags = self.get_img_paths_with_tags()
 
     def get_meta_data(self):
-        meta_fname = os.path.join(ds_path, '_meta.json')
+        meta_fname = os.path.join(self.data_folder, '_meta.json')
         with open(meta_fname, 'r') as f:
             meta_data = f.read()
         meta_data = json.loads(meta_data)
         meta_data = meta_data.get('data')
         return meta_data
 
-    def get_img_paths_with_tags():
+    def get_img_paths_with_tags(self):
         imgs_paths = []
         tags = []
-        for el in self.meta_data():
+        for el in self.meta_data:
             img_id = el.get('id')
-            img_fname = os.path.join(self.data_folder, img_id)
+            img_fname = os.path.join(self.data_folder, img_id + '.jpg')
             img_tag = el.get('tags')
+            imgs_paths.append(img_fname)
+            tags.append(img_tag)
         return imgs_paths, tags
