3efd953cf303fa1da5c2117ca24918b68e3c22c1
Aleks Somov <aleksandr.somov@zennolab.com>
2021-04-22 10:19:08 +0000

    Adding place classifier

---
+++ models/place_classifier.py
@@ -0,0 +1,48 @@
+import torch
+from torch.autograd import Variable as V
+import torchvision.models as models
+from torchvision import transforms as trn
+from torch.nn import functional as F
+import os
+from PIL import Image
+import numpy as np
+
+class PlaceClassifier:
+    def __init__(self):
+        arch = 'resnet18'
+        model_file = '%s_places365.pth.tar' % arch
+        if not os.access(model_file, os.W_OK):
+            weight_url = 'http://places2.csail.mit.edu/models_places365/' + model_file
+            os.system('wget ' + weight_url)
+
+        self.model = models.__dict__[arch](num_classes=365)
+        checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        self.model.load_state_dict(state_dict)
+        self.model.eval()
+        self.centre_crop = trn.Compose([
+                trn.Resize((256,256)),
+                trn.CenterCrop(224),
+                trn.ToTensor(),
+                trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        file_name = 'categories_places365.txt'
+        if not os.access(file_name, os.W_OK):
+            synset_url = 'https://raw.githubusercontent.com/csailvision/places365/master/categories_places365.txt'
+            os.system('wget ' + synset_url)
+        classes = list()
+        with open(file_name) as class_file:
+            for line in class_file:
+                classes.append(line.strip().split(' ')[0][3:])
+        self.classes = tuple(classes)
+        
+    def predict(self, img : np.array, n_answers = 5):
+        img = Image.fromarray(img)
+        input_img = V(self.centre_crop(img).unsqueeze(0))
+        logit = self.model.forward(input_img)
+        h_x = F.softmax(logit, 1).data.squeeze()
+        probs, idx = h_x.sort(0, True)
+        result = {}
+        for i in range(n_answers):
+            result[self.classes[idx[i]]] = probs[i]
+        return result, logit
